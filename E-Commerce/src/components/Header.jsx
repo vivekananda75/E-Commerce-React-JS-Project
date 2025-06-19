46605b0a343fc7d0e47b2d2cc3be8bf093c4dfb7
vivekananda75 <buduruvivek@gmail.com>
2025-06-19 12:07:42 +0530

    seventh commit

--- E-Commerce/src/components/Header.jsx
+++ E-Commerce/src/components/Header.jsx
@@ -1,28 +1,43 @@
 import React from "react";
 import { IoBag } from "react-icons/io5";
-import '../components/Header.css'
+import "../components/Header.css";
 import { IoPerson } from "react-icons/io5";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import productpage from "../pages/productpage";
 import { useSelector } from "react-redux";
 
-
 export default function Header() {
   const cartCount = useSelector((state) => state.cartItems.cart.length);
   return (
     <div className="shadow">
       <header className="d-flex ">
-     <div className="d-flex logo-o">
-    <IoBag className="bag-icon"/> 
-<Link to="/">   <h1 className="logo-text">MART</h1></Link>
-   </div>   
-   <ul className="d-flex nav-links gap-5 ">
-   <Link to="/"><li  className="link-">Home</li></Link>
-   <Link to="Productpage"><li className="link-">Shop</li></Link>
-     <Link to="Cartpage"><li className="link-">Cart</li></Link>
-   <li className="link-"><IoPerson/></li>
-    <li>
+        <div className="d-flex logo-o">
+          <IoBag className="bag-icon" />
+          {/* link 1*/}
+          <Link to="/">
+            {" "}
+            <h1 className="logo-text">MART</h1>
+          </Link>
+        </div>
+        <ul className="d-flex nav-links gap-5 ">
+           {/* link 2*/}
+          <Link to="/">
+            <li className="link-">Home</li>
+          </Link>
+           {/* link 3*/}
+          <Link to="Productpage">
+            <li className="link-">Shop</li>
+          </Link>
+           {/* link 4*/}
+          <Link to="Cartpage">
+            <li className="link-">Cart</li>
+          </Link>
+          <li className="link-">
+            <IoPerson />
+          </li>
+           {/* link 5*/}
+          <li>
             <Link to="Cartpage" className="relative">
               <FaShoppingCart />
               {cartCount > 0 && (
@@ -31,12 +46,9 @@ export default function Header() {
                 </span>
               )}
             </Link>
-          </li>
-   </ul>
-   
-  
-   
-   
+                      
+          </li>
+        </ul>
       </header>
     </div>
   );
